9ab4f75a8a6bc68d334ef16b6c394405c9cce5c4
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-03-31 17:17:21 +0300

    error on migration

---
+++ lib/migrations/versions/497f897492bf_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 497f897492bf
+Revises: 
+Create Date: 2024-03-31 17:13:32.950219
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '497f897492bf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
